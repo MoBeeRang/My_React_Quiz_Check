f321a03649c7f807cadb6fbaffc4ee759fd41d8e
MoBeeRang <preepree@naver.com>
2024-10-16 13:53:25 +0900

    first commit

---
+++ ch06/src/Quiz06_sol.js
@@ -0,0 +1,18 @@
+import React, { useState, useCallback } from 'react'
+
+function Quiz06_sol() {
+   const [count, setCount] = useState(0)
+
+   const increment = useCallback(() => {
+      setCount((prevCount) => prevCount + 1)
+   }, [])
+
+   return (
+      <div>
+         <p>Count: {count}</p>
+         <button onClick={increment}>Increment</button>
+      </div>
+   )
+}
+
+export default Quiz06_sol
